5d5dad6b803f9b0f0b31a801e157422fb7b7e88b
luckyfanghe <fanghexinjian@gmail.com>
2021-11-02 10:24:31 +0800

    fix preview loading issue

--- server.js
+++ server.js
@@ -2,29 +2,29 @@ const express = require("express");
 const path = require("path");
 const dotenv = require('dotenv').config();
 
-const snowflake = require('snowflake-sdk');
-const sfcon = snowflake.createConnection({
-    account: process.env.SNOWFLAKE_ACCOUNT,
-    username: process.env.SNOWFLAKE_USERNAME,
-    password: process.env.SNOWFLAKE_PASSWORD,
-    database: process.env.SNOWFLAKE_DATABASE
-    // authenticator: 'SNOWFLAKE',
-    // clientSessionKeepAlive: true
-});
+// const snowflake = require('snowflake-sdk');
+// const sfcon = snowflake.createConnection({
+//     account: process.env.SNOWFLAKE_ACCOUNT,
+//     username: process.env.SNOWFLAKE_USERNAME,
+//     password: process.env.SNOWFLAKE_PASSWORD,
+//     database: process.env.SNOWFLAKE_DATABASE
+//     // authenticator: 'SNOWFLAKE',
+//     // clientSessionKeepAlive: true
+// });
 
-let connection_ID;
-sfcon.connect( 
-    function(err, conn) {
-        if (err) {
-            console.error('Unable to connect: ' + err.message);
-            } 
-        else {
-            console.log('Successfully connected to Snowflake.');
-            // Optional: store the connection ID.
-            connection_ID = conn.getId();
-        }
-    }
-);
+// let connection_ID;
+// sfcon.connect( 
+//     function(err, conn) {
+//         if (err) {
+//             console.error('Unable to connect: ' + err.message);
+//             } 
+//         else {
+//             console.log('Successfully connected to Snowflake.');
+//             // Optional: store the connection ID.
+//             connection_ID = conn.getId();
+//         }
+//     }
+// );
 
 const app = express();
 
